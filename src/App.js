85e44a52d0b070f6070a5bd08bca33a0ed6ac484
Sai Thikekar <110020678+saithikekar@users.noreply.github.com>
2024-03-31 16:46:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+function App({ signOut, user }) {
+  return (
+    <>
+      <h1>Secure File Transfer {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
